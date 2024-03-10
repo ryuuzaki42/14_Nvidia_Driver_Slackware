8047baebc9dd7c9f9ffadfb1e597d0e14d762884
João Batista <joao42lbatista@gmail.com>
2024-03-10 20:18:47 -0300

    0download_pkg.sh, README.md, and md5sum Nvidia driver (Production Branch Version): "535.154.05" to "550.54.14"

--- 0download_pkg.sh
+++ 0download_pkg.sh
@@ -21,14 +21,14 @@
 #
 # Descrição: Script to download the "last" version of NVIDIA drivers "source"
 #
-# Last update: 14/02/2024
+# Last update: 10/03/2024
 #
 #https://slackbuilds.org/repository/15.0/system/nvidia-kernel/
 #https://slackbuilds.org/repository/15.0/system/nvidia-driver/
 #
 echo -e "\n# Download source to build nvidia driver #"
 
-versionDl="535.154.05"
+versionDl="550.54.14"
 echo "Version: \"$versionDl\""
 linkDl="https://download.nvidia.com/XFree86"
 
@@ -39,12 +39,12 @@ download_x86_64=("$linkDl/Linux-x86_64/$versionDl/NVIDIA-Linux-x86_64-$versionDl
 "$linkDl/nvidia-settings/nvidia-settings-$versionDl.tar.bz2"
 "$linkDl/nvidia-xconfig/nvidia-xconfig-$versionDl.tar.bz2")
 
-md5sum_x86_64=("53ab0bf8076fb5ba881b3a8eab566f72"
-"33f8107a79d1efe4d7274e7d67a5110f"
-"50ffa4dfb4964b8bea7811d252969c2e"
-"4582cddfa5ed98b6c97770fd3b079917"
-"cdb648e0b53ac1c94e61df5ad4012af1"
-"23e89687f0c28ec7d7a0b88c9d802f92")
+md5sum_x86_64=("69e77647875bef192f8458912027e44b"
+"41d2fa0cc25c12f7766648bc9cab4c52"
+"5a78fce16d1a6e06b68ef80a1eded08b"
+"4ad75ba0d9db01708c081c6b8a9c477b"
+"785e1d61781b780779b5d120285837bb"
+"120cbfb926790857e2572a0c0d2087bc")
 
 mkdir -p "source_$versionDl/nvidia-driver/nvidia-kernel"
 cd "source_$versionDl/nvidia-driver/" || exit
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Nvidia driver compiled to Slackware 15.0
 
 ### Version
-    Nvidia driver (Production Branch Version): 535.154.05
+    Nvidia driver (Production Branch Version): 550.54.14
     Slackware 15.0 Kernel: 5.15.145
 
-#### Last update: 14/02/2024
+#### Last update: 10/03/2024
 
 ## Tested in laptops with Nvidia GeForce 930MX and 940MX
     # lspci -v | grep "NVIDIA"
@@ -15,7 +15,7 @@
 1. https://docs.slackware.com/howtos:hardware:nvidia_optimus
 2. https://docs.slackware.com/howtos:hardware:nvidia_optimus#official_optimus_support_with_the_nvidia_proprietary_driver
 3. https://docs.slackware.com/howtos:hardware:proprietary_graphics_drivers
-4. https://download.nvidia.com/XFree86/Linux-x86_64/535.154.05/README/primerenderoffload.html
+4. https://download.nvidia.com/XFree86/Linux-x86_64/550.54.14/README/primerenderoffload.html
 5. https://wiki.archlinux.org/title/NVIDIA
 6. https://wiki.debian.org/NVIDIA%20Optimus
 7. https://www.nvidia.com/object/unix.html
